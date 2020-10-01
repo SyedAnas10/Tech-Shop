3f12b704c4f8d43a7f8aedb06c914cd99fb2b28f
SyedAnas10 <syedanaswaseem@gmail.com>
2020-10-01 09:57:56 +0500

    Dashboard Overhauld

--- client-side/src/Components/Dashboard.js
+++ client-side/src/Components/Dashboard.js
@@ -1,19 +1,33 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Card, CardBody, CardFooter, CardHeader, CardText, CardTitle, Col, Row } from 'reactstrap';
 
-import { fetch_items, fetch_pc_making, fetch_pc_repairing } from '../Redux/ActionCreators';
+import { fetch_items, fetch_pc_making, fetch_pc_making_by_date, fetch_pc_repairing, fetch_pc_repair_by_date, fetch_sales_by_date } from '../Redux/ActionCreators';
 
 let items_fetch = false
 let makeOrders_fetch = false
 let repairOrders_fetch = false
 
+let totalSalesProfit = 0;
+let totalRepairProfit = 0;
+let totalMakeProfit = 0;
+
 function Dashboard() {
 
     const dispatch = useDispatch();
+    const date = new Date()
+    const date_string = date.toString()
+    const month = date_string.slice(4, 7);
+    const day = date_string.slice(8, 10);
+    const year = date_string.slice(11, 15);
     const products = useSelector(state => state.items)
     const lowStocked = products.items.filter(product => product.count <= 10)
     const makeOrders = useSelector(state => state.pc_making)
     const repairOrders = useSelector(state => state.pc_repairing)
+    const Sales = useSelector(state => state.sales_stats)
+    const Repair = useSelector(state => state.pc_repair_stats)
+    const Make = useSelector(state => state.pc_make_stats)
+
     const Center = {
         padding: '20px',
         justifyContent: 'center',
@@ -38,42 +52,118 @@ function Dashboard() {
             repairOrders_fetch = true;
         }
     }, [repairOrders.pc_repairing])
+    useEffect(() => {
+        dispatch(fetch_sales_by_date(day,month,year))
+        totalSalesProfit = 0
+        Sales.stats.forEach(stat => {
+            totalSalesProfit += stat.profit
+        })
+    }, [Sales.stats])
+    useEffect(() => {
+        dispatch(fetch_pc_repair_by_date(day,month,year))
+        totalRepairProfit = 0
+        Repair.stats.forEach(stat => {
+            totalRepairProfit += stat.profit
+        })
+    }, [Repair.stats])
+    useEffect(() => {
+        dispatch(fetch_pc_making_by_date(day,month,year))
+        totalMakeProfit = 0
+        Make.stats.forEach(stat => {
+            totalMakeProfit += stat.profit
+        })
+    }, [Make.stats])
 
 
 {/* FUNCTIONS TO RENDER COUNTS */}
     function itemsCount() {
-        if(lowStocked.length > 0) {
-           return  <p>{ lowStocked.length } items low in stock.</p> 
-        }
-        else {
-            return <p>Currently no item low in stock.</p> 
-        }
+        return(
+            <Card body inverse color= {lowStocked.length === 0 ? 'success':'danger'} >
+                <CardHeader tag='h2'>{lowStocked.length}</CardHeader>
+                <CardBody>
+                    <CardText tag='h4'>Items</CardText>
+                    <CardTitle>in your inventory are low in stock.</CardTitle>
+                </CardBody>
+            </Card>
+        )
     }
     function repairCount() {
-        if(repairOrders.pc_repairing.length > 0) {
-            return <p>{ repairOrders.pc_repairing.length } pending orders.</p>
-        }
-        else {
-            return <p>No pending orders.</p>
-        }
+        return(
+            <Card body inverse color= {repairOrders.pc_repairing.length === 0 ? 'success' : 'danger'} >
+                <CardHeader tag='h2'>{repairOrders.pc_repairing.length}</CardHeader>
+                <CardBody>
+                    <CardText tag='h4'>Orders</CardText>
+                    <CardTitle>in your repair list.</CardTitle>
+                </CardBody>
+            </Card>
+        )
     }
     function makeCount() {
-        if(makeOrders.pc_making.length > 0) {
-            return <p>{ makeOrders.pc_making.length } pending orders.</p>
-        }
-        else {
-            return <p>No pending orders.</p>
-        }
+        return(
+            <Card body inverse color= {makeOrders.pc_making.length === 0 ? 'success' : 'danger'} >
+                <CardHeader tag='h2'>{makeOrders.pc_making.length}</CardHeader>
+                <CardBody>
+                    <CardText tag='h4'>Orders</CardText>
+                    <CardTitle>in your wish list.</CardTitle>
+                </CardBody>
+            </Card>
+        )
+    }
+    function cashIn() {
+        return (
+            <Card className='text-right'>
+                <CardHeader tag='h3'>Today's Cash In</CardHeader>
+                <CardBody>
+                    <CardText tag='h4'>Sales Profit</CardText>
+                    <CardTitle> {totalSalesProfit} </CardTitle>
+                    <CardText tag='h4'>Repairing Profit</CardText>
+                    <CardTitle> {totalRepairProfit} </CardTitle>
+                    <CardText tag='h4'>PC-Make Profit</CardText>
+                    <CardTitle> {totalMakeProfit} </CardTitle>
+                </CardBody>
+                <CardFooter className='text-muted'>{date.toLocaleDateString()}</CardFooter>
+            </Card>
+        )
+    }
+    function cashOut() {
+        return (
+            <Card className='text-left'>
+                <CardHeader tag='h3'>Today's Cash Out</CardHeader>
+                <CardBody>
+                    <CardText tag='h4'>Purchase Expenses</CardText>
+                    <CardTitle> {totalSalesProfit} </CardTitle>
+                    <CardText tag='h4'>Repairing Expenses</CardText>
+                    <CardTitle> {totalRepairProfit} </CardTitle>
+                    <CardText tag='h4'>PC-Make Expenses</CardText>
+                    <CardTitle> {totalMakeProfit} </CardTitle>
+                </CardBody>
+                <CardFooter className='text-muted'>{date.toLocaleDateString()}</CardFooter>
+            </Card>
+        )
     }
 
     return (
         <div style={Center}> 
-            <h3>Low Stocked Items</h3>
-            {itemsCount()}
-            <h3>Repairing Orders</h3>
-            {repairCount()}
-            <h3>PC Orders</h3>
-            {makeCount()}
+        <Row>
+            <Col sm='4'>
+                {itemsCount()}
+            </Col>
+            <Col sm='4'>
+                {repairCount()}
+            </Col>
+            <Col sm='4'>
+                {makeCount()}
+            </Col>
+        </Row>
+        <br></br>
+        <Row>
+            <Col sm='6'>
+                {cashIn()}
+            </Col>
+            <Col sm='6'>
+                {cashOut()}
+            </Col>
+        </Row>
         </div>
     )
 }
