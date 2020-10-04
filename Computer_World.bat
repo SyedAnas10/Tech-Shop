779b3f7ab51928c6e1a3ba39de82b6477a0dd0a9
Mubbashir Ali <mubbashirali35@gmail.com>
2020-10-04 21:27:56 +0500

    Scripts added

--- Computer_World.bat
+++
@@ -1,4 +0,0 @@
-mongod --dbpath "e:\Full Stack Projects\tech-shop-inventory"
-npm start
-cd client-side
-yarn start
---
+++ Run.bat
@@ -0,0 +1,3 @@
+start "./Scripts/db_connection.bat" /min cmd /k call "./Scripts/db_connection.bat"
+start "./Scripts/server_start.bat" /min cmd /k call "./Scripts/server_start.bat"
+start "./Scripts/client_start.bat" /min cmd /k call "./Scripts/client_start.bat"
